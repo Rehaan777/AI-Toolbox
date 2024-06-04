8a2173828aa60ce28ef683ade0c0e24240142853
Gurnoor OffSec <89371587+Rehaan777@users.noreply.github.com>
2024-06-04 11:46:16 +0530

    Add files via upload

---
+++ AI-Toolbox.py
@@ -0,0 +1,117 @@
+import time
+
+print("Welcome to the Premium Auto AI Tool")
+time.sleep(2)
+print ("Are you ready to explore the world of AI?")
+
+time.sleep(3)
+print("            _____               .___             ")
+print("           /     \  _____     __| _/ ____        ")
+print("          /  \ /  \ \__  \   / __ |_/ __ \       ")
+print("         /    Y    \ / __ \_/ /_/ |\  ___/       ")
+print("         \____|__  /(____  /\____ | \___  >      ")
+print("          \/      \/      \/     \/              ")
+
+print("         __________                              ")
+print("         \______   \ ___.__.                     ")
+print("          |    |  _/<   |  |                     ")
+print("          |    |   \ \___  |                     ")
+print("          |______  / / ____|                     ")
+print("                 \/  \/                          ")
+
+print("           ________                                                ")
+print("          /  _____/  __ __ _______   ____    ____    ____ _______  ")
+print("         /   \  ___ |  |  ""\"\_  __ \ /    \  /  _ \  /  _ _  __ \ ")
+print("         \    \_\  \|  |  / |  | \/|   |  \(  <_> )(  <_> )|  | \/ ")
+print("          \______  /|____/  |__|   |___|  / \____/  \____/ |__|    ")
+print("                 \/                     \/                         ")
+time.sleep(3)
+
+print("1. Writing Assistant")
+print("2. Conversational AI")
+print("3. Content Creation")
+print("4. Image Generation")
+print("5. Video Creation")
+print("6. Language Translation")
+print("7. Video Creation")
+print("8. Transcription")
+print("9. Personal AI Companion")
+print("10.Audio and Video Editing")
+print("11.AI Video Editing")
+print("12.Copywriting")
+print("13.Storytelling")
+print("14.Presentations")
+print("15.Interactive Fiction")
+print("16.Voiceover")
+print("17.Chatbots")
+print("18.Photo Editing")
+print("19.Video Creation")
+print("20.Image Generation")
+
+while True:
+
+   tools=int(input("Please enter the corresponding number: "))
+   time.sleep(2)
+   if tools == 1:
+    print("Grammarly can help - https://www.grammarly.com/")
+    break
+   elif tools == 2:
+    print("ChatGPT can help - https://chat.openai.com/")
+    break
+   elif tools == 3:
+    print("Jasper can help - https://www.jasper.ai/")
+    break
+   elif tools == 4:
+    print("DALL-E can help - https://www.openai.com/dall-e-2")
+    break
+   elif tools == 5:
+    print("Synthesia can help - https://www.synthesia.io/")
+    break
+   elif tools == 6:
+    print("DeepL can help - https://www.deepl.com/")
+    break
+   elif tools == 7:
+    print("Lumen5 can help - https://www.lumen5.com/")
+    break
+   elif tools == 8:
+    print("Otter.ai can help - https://otter.ai/")
+    break
+   elif tools == 9:
+    print("Replika can help - https://replika.ai/")
+    break
+   elif tools == 10:
+    print("Descript can help - https://www.descript.com/")
+    break
+   elif tools == 11:
+    print("RunWayML can help - https://runwayml.com/")
+    break
+   elif tools == 12:
+    print("CopyAI can help - https://www.copy.ai/")
+    break
+   elif tools == 13:
+    print("Tome can help - https://tome.app/")
+    break
+   elif tools == 14:
+    print("Beautiful.ai can help - https://www.beautiful.ai/")
+    break
+   elif tools == 15:
+    print("AI Dungeon can help - https://play.aidungeon.com/")
+    break
+   elif tools == 16:
+    print("Murf.ai can help - https://murf.ai/")
+    break
+   elif tools == 17:
+    print("Chatbot.com can help - https://www.chatbot.com/")
+    break
+   elif tools == 18:
+    print("Fotor can help - https://www.fotor.com/")
+    break
+   elif tools == 19:
+    print("Pictory can help - https://pictory.ai/")
+    break
+   elif tools == 20:
+    print("DeepDream Generator can help - https://deepdreamgenerator.com/")
+    break
+   else:
+    print("Invalid number. Please enter a number from 1-20")
+
